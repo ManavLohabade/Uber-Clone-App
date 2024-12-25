a64ad53c3979d0f46488ce56c4ae9a64fd9a7136
ManavLohabade <manavlohabade26@gmail.com>
2024-12-25 23:57:16 +0530

    Updated Code

--- Backend/models/controllers/user.controller.js
+++ Backend/models/controllers/user.controller.js
@@ -1,6 +1,10 @@
 const userModels = require('../models/user.model');
+const userServices = require('../services/user.services');
 
 
 module.exports.registerUser = async (req, res, next) => {
-try{}
+    
 }
+
+
+
--- Backend/services/user.services.js
+++ Backend/services/user.services.js
@@ -2,4 +2,20 @@ const userModel = require('../models/user.models');
 
 
 
-module.exports.createUser = async ({})
+module.exports.createUser = async ({
+    fullname,
+    email,
+    password
+})  => {
+    if (!firstname || !email || !password) {   
+        throw new Error ('All fields are required');
+    }
+    const user = userModels.create({
+        fullname: { 
+            firstname,
+            lastname
+        },
+        email,
+        password
+    });
+}
